0347de2b20f05e92b164f1d8f8f459b089236877
unknown <gustavocastruu@gmail.com>
2026-07-13 00:12:00 -0300

    Update SQL Workbeach

---
+++ Banco de Dados/main02.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Gucastro1",
+    database="banco"   # nome do banco
+)
+
+print("Conectado com sucesso!")
+
+cursor = conexao.cursor()
+
+cursor.execute("Select * from locais")
+
+resultado = cursor.fetchall()
+
